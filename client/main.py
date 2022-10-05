c0126e49f3746895202433badddd96f410d8592c
BAaboe <benjamin.mjaatvedt@gmail.com>
2022-10-05 22:46:29 +0200

    added a better view of the tasks, but dosnt wokr!

--- client/main.py
+++ client/main.py
@@ -2,8 +2,16 @@ import mysql.connector as mc
 import os
 from time import sleep
 from playsound import playsound
+from rich.live import Live
+from rich.table import Table
 
 os.system("clear");
+
+table = Table()
+table.add_column("Who")
+table.add_column("Where")
+table.add_column("Why")
+
 
 host = "localhost"
 database = "support"
@@ -11,32 +19,35 @@ user = "techsupport"
 password = "goin"
 
 
-prevRecord = None
-while True:
+with Live(table, refresh_per_second=4):
+    prevElement = None
+    while True:
 
-    connection = mc.connect(host=host, database=database, user=user, password=password)
-    if connection.is_connected():
-        cursor = connection.cursor()
+        connection = mc.connect(host=host, database=database, user=user, password=password)
+        if connection.is_connected():
+            cursor = connection.cursor()
 
-        #sql = "INSERT INTO tasks (pname, room ,pdesc) VALUES (%s, %s, %s)"
-        #val = ("TEST name", "TEST room", "TEST description")
-        #
-        #cursor.execute(sql, val)
-        #connection.commit()
+            #sql = "INSERT INTO tasks (pname, room ,pdesc) VALUES (%s, %s, %s)"
+            #val = ("TEST name", "TEST room", "TEST description")
+            #
+            #cursor.execute(sql, val)
+            #connection.commit()
 
-        cursor.execute("SELECT * FROM tasks")
-        record = cursor.fetchall()
-
-        if(record != prevRecord):
-            os.system("clear")
-            for x in record:
-                print(f"'{x[1]}' needs help in '{x[2]} with '{x[3]}'")
-            playsound("assets/notify.wav")
-            prevRecord = record
-    else:
-        break
-    connection.close()
-    sleep(1)
+            cursor.execute("SELECT * FROM tasks")
+            record = cursor.fetchall()
+            if prevElement == None:
+                for i in record:
+                    table.add_row(i[1], i[2], i[3])
+                prevElement = record[-1]
+                continue
+            if prevElement != record[-1]:
+                table.add_row(record[-1][1], record[-1][2], record[-1][3])
+                playsound("assets/notify.wav")
+                prevElement = record[-1]
+        else:
+            break
+        connection.close()
+        sleep(1)
 
 connection.close()
 
